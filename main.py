2f75ef4f119ab2d9315c27c8b608e6104e4ca991
hecv14 <hectormvl@gmail.com>
2025-09-25 21:48:56 +0200

    added type annotation, tried to increase readability with lambda fs which capture div. by zero, improve f-strings

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # %%
 import warnings
 
-def simple_power(wsp_input, pow_rated = 15,
-                 wsp_cutin = 3, wsp_rated = 11, wsp_cutout = 25,
-                 interp_method="linear"):
+def simple_power(wsp_input: float | int,
+                pow_rated: float | int = 15,
+                wsp_cutin: float | int = 3,
+                wsp_rated: float | int = 11,
+                wsp_cutout: float | int = 25,
+                interp_method: str ="linear"
+                ) -> float | int:
     """simple_power is used to roughly interpolate the power in the curve of a
     variable speed WTG. A linear and a cubic interpolation option are given.
 
@@ -18,21 +22,32 @@ def simple_power(wsp_input, pow_rated = 15,
     Returns:
         pow_out: interpolated power for the wsp_input, same units as pow_rated.
     """
+    # weighting if the interpolation is linear
+    g_lin = lambda wsp_input, wsp_cutin, wsp_rated: \
+    (wsp_input - wsp_cutin) / (wsp_rated - wsp_cutin) \
+    if wsp_rated != wsp_cutin else float('inf')
+
+    # if it is cubic
+    g_cub = lambda wsp_input, wsp_rated: \
+    (wsp_input**3 )/(wsp_rated**3) \
+    if wsp_rated != 0 else float('inf')
+
     # no need to interpolate below wsp_cutin
     if 0 <= wsp_input < wsp_cutin:
         pow_out = 0
     elif wsp_cutin <= wsp_input < wsp_rated:
+        interp_method = interp_method.lower().strip() # clean the string a bit
         # using match case here to avoid too many ifs
         match interp_method:
             case "linear":
                 # g_fwsp is the weighting function pow_out / pow_rated as f(wsp)
-                g_fwsp = (wsp_input - wsp_cutin)/(wsp_rated - wsp_cutin)
+                g_fwsp = g_lin(wsp_input, wsp_cutin, wsp_rated)
             case "cubic":                
-                g_fwsp = (wsp_input**3 )/(wsp_rated**3)
+                g_fwsp = g_cub(wsp_input, wsp_rated)
             case _:
-                warnings.warn('Interpolation method not recognized will use ' \
-                '"linear"')
-                g_fwsp = (wsp_input - wsp_cutin)/(wsp_rated - wsp_cutin)
+                warnings.warn("Interpolation method unrecognized, using linear")
+                g_fwsp = g_lin(wsp_input, wsp_cutin, wsp_rated)
+        # with the weighting factor find the power output
         pow_out = g_fwsp * pow_rated
     elif wsp_rated <= wsp_input < wsp_cutout:
         pow_out = pow_rated
@@ -45,19 +60,22 @@ def simple_power(wsp_input, pow_rated = 15,
 if __name__ == '__main__':
     # Write the main script to use the function here:
     # test the function with wind speeds from 0 to 1m/s above cut-out
+    print('using linear interpolation.')
     for wsp in range(26):
         pow = simple_power(wsp) # linear
-        print(f"{wsp}, {pow}")
+        print(f"{wsp}, {pow:,.2f}")
     print('\n')
 
+    print('using cubic interpolation.')
     for wsp in range(26):
         pow = simple_power(wsp, interp_method="cubic")
-        print(f"{wsp}, {pow}")
+        print(f"{wsp}, {pow:,.2f}")
     print('\n')
 
+    print('using wrong arg. for interpolation.')
     for wsp in range(26):
         pow = simple_power(wsp, interp_method="typo") # test bad input
-        print(f"{wsp}, {pow}")
+        print(f"{wsp}, {pow:,.2f}")
     print('\n')
 
 # %%
